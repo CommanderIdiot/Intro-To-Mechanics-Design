0fff88d71d5e2182b858969c047555cb12136757
CommanderIdiot <michaelgw06@outlook.com>
2025-10-03 11:47:38 +0100

    V0.1
    IMCD Tutorial 2

---
+++ IMDFramework/Assets/Assessment 1 Scripts/CharacterMovement.cs
@@ -0,0 +1,89 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMovement : MonoBehaviour
+{
+	private Rigidbody2D m_RB;
+
+	[SerializeField] private float m_MoveSpeed;
+	[SerializeField] private float m_JumpStrength;
+	[SerializeField] private Transform m_RaycastPosition;
+	[SerializeField] private LayerMask m_GroundLayer;
+	private float m_InMove;
+    private PlayerControls m_ActionMap;
+    private bool m_IsGrounded;
+
+    [SerializeField] private float m_CoyoteTimeThreashold = 0.4f;
+    private float m_CoyoteTimeCounter;
+
+    private void Awake()
+    {
+        m_ActionMap = new PlayerControls();
+
+		m_RB = GetComponent<Rigidbody2D>();
+    }
+
+    #region Bindings
+    private void OnEnable()
+    {
+        m_ActionMap.Enable();
+
+        m_ActionMap.Default.MoveHoriz.performed += Handle_MovePerformed;
+        m_ActionMap.Default.MoveHoriz.canceled += Handle_MoveCancelled;
+        m_ActionMap.Default.Jump.performed += Handle_JumpPerformed;
+    }
+
+    private void OnDisable()
+    {
+        m_ActionMap.Disable();
+
+        m_ActionMap.Default.MoveHoriz.performed -= Handle_MovePerformed;
+        m_ActionMap.Default.MoveHoriz.canceled -= Handle_MoveCancelled;
+        m_ActionMap.Default.Jump.performed -= Handle_JumpPerformed;
+    }
+    #endregion
+
+    #region InputFunctions
+
+    private void Handle_MovePerformed(InputAction.CallbackContext context)
+    {
+        m_InMove = context.ReadValue<float>();
+    }
+    private void Handle_MoveCancelled(InputAction.CallbackContext context)
+    {
+        m_InMove = 0f;
+    }
+
+    private void Handle_JumpPerformed(InputAction.CallbackContext context)
+    {
+        if (m_IsGrounded || m_CoyoteTimeCounter > 0)
+        {
+            m_RB.AddForce(Vector2.up * m_JumpStrength, ForceMode2D.Impulse);
+            m_CoyoteTimeCounter = 0;
+        }
+    }
+    #endregion
+
+    private void Update()
+    { 
+        /* Potentially have it set as coroutines or timers to avoid using update?
+        Also, possibly do similar for jump buffering, but will only jump if the jump key was pressed within an x time limit?
+        */ 
+        if (m_IsGrounded)
+        {
+            m_CoyoteTimeCounter = m_CoyoteTimeThreashold;
+        }
+        else
+        {
+            m_CoyoteTimeCounter -= Time.deltaTime;;
+        }
+    }
+
+    private void FixedUpdate()
+	{
+		m_RB.linearVelocityX = m_MoveSpeed * m_InMove;
+
+        m_IsGrounded = Physics2D.Raycast(m_RaycastPosition.position, Vector2.down, 0.1f, m_GroundLayer);
+	}
+}
